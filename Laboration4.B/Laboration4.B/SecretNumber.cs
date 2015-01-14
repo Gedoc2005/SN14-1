e30b2e4c824214269f0bddc7ed2be8e0fb752825
Nils Nordal <mb1nonil@hotmail.com>
2015-01-14 12:12:59 +0100

    Finish och lade till kommentarer.

--- Laboration4.B/Laboration4.B/SecretNumber.cs
+++ Laboration4.B/Laboration4.B/SecretNumber.cs
@@ -9,66 +9,53 @@ namespace Laboration4.B
     public class SecretNumber
     {
         private int[] _guessedNumbers;
-        private int _number;
-        private int _count;
-        private bool _canMakeGuess;
+        private int _number; //Det hemliga numret.
         public const int MaxNumberOfGuesses = 7;
 
-        //Behöver jag sätta get som public specifikt och behöver jag lägga till fält, behöver jag kunna getta?
-        public bool CanMakeGuess { get { return _canMakeGuess; } private set { _canMakeGuess = value; } }
-        public int Count 
+        public bool CanMakeGuess { get; private set; }
+        public int Count { get; private set; } //Lagrar antalet frågor
+        public int GuessesLeft 
         { 
-            get { return _count; } 
-            private set 
-            {
-                if (value >= MaxNumberOfGuesses)
-                {
-                    throw new ApplicationException();
-                }
-                _count = value;
-            } 
+            get { return -1 * (Count - MaxNumberOfGuesses); } 
         }
-        public int GuessesLeft { get { return -1 * (Count - MaxNumberOfGuesses); } }
 
         public void Initialize()
         {
+            //Se till att vi inte lagrar några värden i _guessedNumbers:
             Array.Clear(_guessedNumbers, 0, _guessedNumbers.Length);
 
+            //Initiera det hemliga numret:
             Random random = new Random();
             _number = random.Next(1, 101);
 
+            //Återställ fälten:
             CanMakeGuess = true;
             Count = 0;
         }
         public bool MakeGuess(int number)
         {
-
-            //Kolla är godkända:
-            if (number < 1 || number > 100)
+            //Kontrollera om vi får göra fler gissningar och att argumentet är godkänt 
+            if (Count >= MaxNumberOfGuesses)
+            {
+                throw new ApplicationException();
+            }
+            else if (number < 1 || number > 100)
             {
                 throw new ArgumentOutOfRangeException();
             }
 
-            int a = Array.IndexOf(_guessedNumbers, number);//tillfälligt
-            if (a > -1)
+            //Om gissningen är unik => spara gissningen och inkrementera "frågeräknaren":
+            if (Array.IndexOf(_guessedNumbers, number) > -1)
             {
                 Console.WriteLine("Du har redan gissat på {0}. Gör om gissningen!", number);
                 return false;
             }
-            _guessedNumbers[Count] = number;
-
-
-
-            try
+            else
             {
-                Count++; //inkrementera "svarsräknaren".
+                _guessedNumbers[Count] = number;
+                Count++;
             }
-            catch (ApplicationException)
-            {
-                Console.WriteLine("Det hemliga talet är {0}", _number);
-                CanMakeGuess = false;
-            }
-
+            
             //Skriv ut respektive svar beroende på argumenet/gissningen:
             if (number == _number)
             {
@@ -83,25 +70,23 @@ namespace Laboration4.B
             }
             else if (number > _number)
             {
-                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar. ",
-                    number, GuessesLeft);
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar. ", number, GuessesLeft);
             }
 
             //Lägg till detta i utskriften när man gissat max antal gånger:
-            //if (Count == MaxNumberOfGuesses)//Är detta uttryck överflödigt, kolla countprop
-            //{
-            //    Console.WriteLine("Det hemliga talet är {0}", _number);
-            //    CanMakeGuess = false;
-            //}
+            if (Count == MaxNumberOfGuesses)//Är detta uttryck överflödigt, kolla countprop
+            {
+                Console.WriteLine("Det hemliga talet är {0}", _number);
+                CanMakeGuess = false;
+            }
             return false;
         }
+
         public SecretNumber()
         {
+            //Initiera objektet:
             _guessedNumbers = new int[MaxNumberOfGuesses];
             Initialize();
         }
-
-
-
     }
 }
