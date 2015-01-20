abd1ccb92356e8d2488fecf4a225667ad56fb2db
Nils Nordal <mb1nonil@hotmail.com>
2015-01-20 16:56:31 +0100

    början

---
+++ KylskapA/Cooler.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KylskapA
+{
+    public class Cooler//todo ska den modifieras till public?
+    {
+        private decimal _insideTemperature;
+        private decimal _targetTemperature;
+        public const decimal OutsideTemperature = 23.7M;
+
+        public bool DoorIsOpen { get; set; }
+        public decimal InsideTemperature 
+        { 
+            get
+            {
+                return _insideTemperature;
+            }
+            set
+            {
+                if (value < 0 || value > 45)// TODO: Kolla om jag ska kunna skicka in 0 och 45?
+                {
+                    throw new ArgumentException("Innertemperaturen är inte i intervallet 0 - 45°C");
+                }
+                else if (value < _targetTemperature)
+                {
+                    _insideTemperature = _targetTemperature;// todo jävligt osäker på detta är rätt!
+                }
+                else if (value > OutsideTemperature)
+                {
+                    _insideTemperature = OutsideTemperature;
+                }
+                else
+                {
+                    _insideTemperature = value; 
+                }
+            }
+        }
+        public bool IsOn { get; set; }
+        public decimal TargetTemperature 
+        { 
+            get
+            {
+                return _targetTemperature;
+            }
+            set
+            {
+                if (value < 0 || value > 20)// TODO: Kolla om jag ska kunna skicka in 0 och 20? Är det rätt värden?
+                {
+                    throw new ArgumentException("Måltemperaturen är inte i intervallet 0 - 20°C");
+                }
+                _targetTemperature = value;
+            }
+        }
+
+        public Cooler()
+            : this(0M, 0M)
+        {
+
+        }
+        public Cooler(decimal temperature, decimal targetTemperature)
+            : this(temperature, targetTemperature, false, false)
+        {
+
+        }
+        public Cooler(decimal temperature, decimal targetTemperature, bool isOn, bool doorIsOpen)
+        {
+            InsideTemperature = temperature;// todo rätt egenskap?
+            TargetTemperature = targetTemperature;
+            IsOn = isOn;
+            DoorIsOpen = doorIsOpen;
+        }
+
+        public void Tick()
+        {
+            if (IsOn)
+            {
+                if (DoorIsOpen)
+	            {
+		            InsideTemperature += 0.2M; 
+	            }
+                else
+	            {
+                    InsideTemperature -= 0.2M;
+	            }
+            }
+            else
+            {
+                if (DoorIsOpen)
+                {
+                    InsideTemperature += 0.5M;
+                }
+                else
+                {
+                    InsideTemperature += 0.1M;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            //todo formatera detta korrekt!
+            //tex 2 decimaler
+            //behöver jag en variabel?
+            string returnValue;
+            returnValue = String.Format("[{0}] : {1:F1}°C : ({2:F1}°C) - {3}",
+                IsOn == true ? "PÅ" : "AV",
+                InsideTemperature,
+                TargetTemperature,
+                DoorIsOpen == true ? "Öppet" : "Stängt");
+            return returnValue;
+        }
+
+
+    }
+}
---
+++ KylskapA/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KylskapA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////1
+            Cooler cooler = new Cooler();
+            //Console.WriteLine(cooler.ToString());
+            ////2
+            //cooler = new Cooler(24.5M, 4M);
+            //Console.WriteLine(cooler.ToString());
+            ////3
+            //cooler = new Cooler(19.5M, 4M, true, false);
+            //Console.WriteLine(cooler.ToString());
+            //4
+            //cooler = new Cooler(5.3M, 4M, true, false);
+            //Console.WriteLine(cooler.ToString());
+            //Run(cooler, 10);
+            //5
+            //cooler = new Cooler(5.3M, 4M, false, false);
+            //Console.WriteLine(cooler.ToString());
+            //Run(cooler, 10);
+            //6
+            //cooler = new Cooler(5.3M, 4M, true, true);
+            //Console.WriteLine(cooler.ToString());
+            //Run(cooler, 10);
+            //7
+            //cooler = new Cooler(19.7M, 4M, false, true);
+            //Console.WriteLine(cooler.ToString());
+            //Run(cooler, 10);
+            //8
+            //try
+            //{
+            //    cooler.InsideTemperature = 100M;//argument
+            //}
+            //catch (ArgumentException ex)
+            //{
+            //    ViewErrorMessage(ex.Message);
+            //}
+
+            //try
+            //{
+            //    cooler.TargetTemperature = 100M;
+            //}
+            //catch (ArgumentException ex)
+            //{
+            //    ViewErrorMessage(ex.Message);
+            //}
+            //9
+            try
+            {
+                cooler = new Cooler(100M, 100M, false, true);
+            }
+            catch (ArgumentException ex)
+            {
+                ViewErrorMessage(ex.Message);
+            }
+
+
+
+
+
+
+        }
+
+        private static void Run(Cooler c, int minutes)
+        {
+            for (int ticks = 0; ticks < minutes; ticks++)
+            {
+                
+                c.Tick();
+                Console.WriteLine(c.ToString());
+            }
+        }
+        private static void ViewErrorMessage(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        private static void ViewTestHeader(string header)
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine("Test nummer.");
+            Console.WriteLine(header);
+        }
+        private static void Test()
+        {
+
+        }
+    }
+}
