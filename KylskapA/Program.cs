b68b1469139bf4a78e6eb3b1fcfdb51f13827bb0
unknown <nils.nordal@R9ZBVN2.learnet.se>
2015-01-21 12:51:43 +0100

    testet är klart

--- KylskapA/Program.cs
+++ KylskapA/Program.cs
@@ -11,92 +11,127 @@ namespace KylskapA
         static void Main(string[] args)
         {
             Cooler cooler;
-            //1
-            cooler = new Cooler();
-            Console.WriteLine(cooler.ToString());
-            //2
-            cooler = new Cooler(24.5M, 4M);
-            Console.WriteLine(cooler.ToString());
-            //3
-            cooler = new Cooler(19.5M, 4M, true, false);
-            Console.WriteLine(cooler.ToString());
-            //4
-            cooler = new Cooler(5.3M, 4M, true, false);
-            Console.WriteLine(cooler.ToString());
-            Run(cooler, 10);
-            //5
-            cooler = new Cooler(5.3M, 4M, false, false);
-            Console.WriteLine(cooler.ToString());
-            Run(cooler, 10);
-            //6
-            cooler = new Cooler(5.3M, 4M, true, true);
-            Console.WriteLine(cooler.ToString());
-            Run(cooler, 10);
-            //7
-            cooler = new Cooler(19.7M, 4M, false, true);
-            Console.WriteLine(cooler.ToString());
-            Run(cooler, 10);
-            //8
-            //try
-            //{
-            //    cooler.InsideTemperature = 100M;//argument
-            //}
-            //catch (ArgumentException ex)
-            //{
-            //    ViewErrorMessage(ex.Message);
-            //}
 
-            //try
-            //{
-            //    cooler.TargetTemperature = 100M;
-            //}
-            //catch (ArgumentException ex)
-            //{
-            //    ViewErrorMessage(ex.Message);
-            //}
-            //9
-            //try
-            //{
-            //    cooler = new Cooler(100M, 100M, false, true);
-            //}
-            //catch (ArgumentException ex)
-            //{
-            //    ViewErrorMessage(ex.Message);
-            //}
-            
-            for (int testNumber = 1; testNumber <= 10; testNumber++)
+            for (int testNumber = 1; testNumber < 10; testNumber++)// todo finns det något bättre testalternativ?
             {
+                Console.WriteLine("════════════════════════════════════════");
+                Console.WriteLine("Test {0}.", testNumber);
+
                 switch (testNumber)
                 {
                     case 1:
+                        Console.WriteLine("Test av standardkonstruktorn\n");
                         cooler = new Cooler();
+                        Console.WriteLine(cooler.ToString());
                         break;
                     case 2:
+                        Console.WriteLine("Test av konstruktorn med två parametrar, (24,5 och 4)\n");
                         cooler = new Cooler(24.5M, 4M);
+                        Console.WriteLine(cooler.ToString());
                         break;
                     case 3:
+                        Console.WriteLine("Test av konstruktorn med 4 parametrar, (19,5, 4, True, False)\n");
                         cooler = new Cooler(19.5M, 4M, true, false);
+                        Console.WriteLine(cooler.ToString());
                         break;
                     case 4:
+                        Console.WriteLine("Test av kylning med metoden Tick\n");
                         cooler = new Cooler(5.3M, 4M, true, false);
+                        Run(cooler, 10);
+                        Console.WriteLine(cooler.ToString());
                         break;
                     case 5:
+                        Console.WriteLine("Test av avstängt kylskåp med metoden Tick, vara avslaget och ha stängd dörr\n");
                         cooler = new Cooler(5.3M, 4M, false, false);
+                        Run(cooler, 10);
+                        Console.WriteLine(cooler.ToString());
                         break;
                     case 6:
+                        Console.WriteLine("Test av påslaget kylskåp med metoden Tick, vara på och ha öppen dörr\n");
                         cooler = new Cooler(5.3M, 4M, true, true);
+                        Run(cooler, 10);
+                        Console.WriteLine(cooler.ToString());
                         break;
                     case 7:
+                        Console.WriteLine("Test av avslaget kylskåp med metoden Tick, ha öppen dörr\n");
                         cooler = new Cooler(19.7M, 4M, false, true);
+                        Run(cooler, 10);
+                        Console.WriteLine(cooler.ToString());
                         break;
                     case 8:
-                        
+                        Console.WriteLine("Test av egenskaperna så att undantag kastas då innertemperaturen och måltemperaturen tilldelas felaktiga värden\n");
+                        cooler = new Cooler();
+                        try
+                        {
+                            cooler.InsideTemperature = 46M;
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            ViewErrorMessage(ex.Message);
+                        }
+                        try
+                        {
+                            cooler.InsideTemperature = -1M;
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            ViewErrorMessage(ex.Message);
+                        }
+                        try
+                        {
+                            cooler.TargetTemperature = 21M;
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            ViewErrorMessage(ex.Message);
+                        }
+                        try
+                        {
+                            cooler.TargetTemperature = -1M;
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            ViewErrorMessage(ex.Message);
+                        }
                         break;
                     case 9:
-                        
+                        Console.WriteLine("Test av konstruktorerna så att undantag kastas då innertemperaturen och måltemperaturen tilldelas felaktiga värden\n");
+                        cooler = new Cooler();
+                        try
+                        {
+                            cooler = new Cooler(46M, 0M);
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            ViewErrorMessage(ex.Message);
+                        }
+                        try
+                        {
+                            cooler = new Cooler(-1M, 0M);
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            ViewErrorMessage(ex.Message);
+                        }
+                        try
+                        {
+                            cooler = new Cooler(0M, 21M);
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            ViewErrorMessage(ex.Message);
+                        }
+                        try
+                        {
+                            cooler = new Cooler(0M, -1M);
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            ViewErrorMessage(ex.Message);
+                        }
                         break;
                 }
-
+                Console.WriteLine();
 
             }
 
@@ -120,25 +155,11 @@ namespace KylskapA
             Console.WriteLine(message);
             Console.ResetColor();
         }
-        private static void ViewTestHeader(string header)
+        private static void ViewTestHeader()
         {
-            Console.WriteLine("=================================");
-            Console.WriteLine("Test nummer.");
-            Console.WriteLine(header);
-        }
-        private static void Test()
-        {
-
-        }
-        private static void TryCatch()
-        {
-            try
+            for (int column = 0; column < 40; column++)
             {
-
-            }
-            catch (ArgumentException ex)
-            {
-                ViewErrorMessage(ex.Message);
+                Console.Write((char)205);
             }
         }
     }
